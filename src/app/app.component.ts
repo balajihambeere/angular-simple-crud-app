f4c2d22d5be4c1dab8ea5167e87f8ae824518f8a
ankita hambeere <ankita.hambeere@hotmail.com>
2018-09-02 13:05:10 +0530

    Added constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { CustomerService } from './customer.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'AngularCRUDApp';
+  constructor() { }
 }
